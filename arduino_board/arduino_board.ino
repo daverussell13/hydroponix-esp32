41939c658079aaa29b9a84a8aba04d506f909d9a
Dave Russell <daverussell1309@gmail.com>
2023-12-10 09:54:32 +0700

    changes: esp32 handling sensor, arduino handling pump triggering

--- arduino_board/arduino_board.ino
+++ arduino_board/arduino_board.ino
@@ -1,142 +1,40 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
 
-SoftwareSerial Esp32SerialCom(2, 3); // rx, tx
+SoftwareSerial ArduinoSerialCom(2, 3);
 
-const int pinRelay = 7;
-
-// TDS
-#define TdsSensorPin A0
-#define VREE 5.0  // Sesuaikan tegangan referensi sesuai kebutuhan
-#define SCOUNT 30
-
-int analogBuffer[SCOUNT];
-int analogBufferTemp[SCOUNT];
-int analogBufferIndex = 0;
-int copyIndex = 0;
-float averageVoltage = 0;
-float tdsValue = 0;
-
-// PH
-const int pin_pH = A1;  // Sesuaikan pin analog PH
-int pH_analog;
-float Po = 0;
-float pH_step;
-double voltage;
-float PH4 = 3.22;
-float PH7 = 2.69;
-
-// Temp
-const int oneWireBus = 8;
-OneWire oneWire(oneWireBus);
-DallasTemperature sensors(&oneWire);
+// Pump relay
+const int tdsBufferRelay = 7;
+const unsigned long tdsRelayDuration = 2000; // 2 seconds
 
 void setup() {
   Serial.begin(9600);
-  Esp32SerialCom.begin(9600);
-
-  pinMode(TdsSensorPin, INPUT);
-  sensors.begin();
-  pinMode(pinRelay, OUTPUT);
-
-  // Matikan pompa pada awalnya
-  digitalWrite(pinRelay, LOW);
+  ArduinoSerialCom.begin(9600);
+  pinMode(tdsBufferRelay, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(tdsBufferRelay, HIGH);
 }
 
 void loop() {
-  // Temp
-  sensors.requestTemperatures();
-  float temperatureC = sensors.getTempCByIndex(0);
-  Serial.print(temperatureC);
-  Serial.println("ºC");
-
-  // TDS
-  static unsigned long analogSampleTime = millis();
-  if (millis() - analogSampleTime > 40U) {
-    analogSampleTime = millis();
-    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);
-    analogBufferIndex++;
-    if (analogBufferIndex == SCOUNT) {
-      analogBufferIndex = 0;
-    }
-  }
-
-  static unsigned long printTimePoint = millis();
-  if (millis() - printTimePoint > 800U) {
-    printTimePoint = millis();
-    for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++) {
-      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
-      averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREE / 1023.0;  // Untuk Arduino Uno, ADC resolution adalah 10 bits (1023)
-      float compensationCoefficient = 1.0 + 0.02 * (temperatureC - 25.0);
-      float compensationVoltage = averageVoltage / compensationCoefficient;
-      tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
-      Serial.print("TDS Value: ");
-      Serial.print(tdsValue, 0);
-      Serial.println(" ppm");
-    }
-  }
-
-  // PH
-  pH_analog = analogRead(pin_pH);
-  Serial.print("ADC Value: ");
-  Serial.println(pH_analog);
-
-  // Calculate voltage
-  voltage = pH_analog * (5.0 / 1023.0);  // Untuk Arduino Uno, ADC resolution adalah 10 bits (1023)
-  Serial.print("Voltage: ");
-  Serial.println(voltage, 4);
-
-  // Calculate pH value of the liquid
-  pH_step = (PH4 - PH7) / 3;
-  Po = 9.4 + ((PH7 - voltage) / pH_step);
-  Serial.print("pH Value: ");
-  Serial.println(Po, 2);
-  Serial.println();
-
-  // Kirim data secara serial
-  DynamicJsonDocument doc(JSON_OBJECT_SIZE(3));
-
-  doc["temp"] = temperatureC;
-  doc["tds"] = tdsValue;
-  doc["ph"] = Po;
-
-  String jsonString;
-  serializeJson(doc, jsonString);
-
-  Esp32SerialCom.println(jsonString);
-
-  // digitalWrite(pinRelay, HIGH);
-  // Serial.println("Pompa ON");
-  // delay(5000); // Delay 5 detik
-
-  // // Matikan pompa selama 5 detik
-  // digitalWrite(pinRelay, LOW);
-  // Serial.println("Pompa OFF");
-  // delay(5000); 
-}
-
-int getMedianNum(int bArray[], int iFilterLen) {
-  int bTab[iFilterLen];
-  for (byte i = 0; i < iFilterLen; i++) {
-    bTab[i] = bArray[i];
-  }
-  int i, j, bTemp;
-  for (j = 0; j < iFilterLen - 1; j++) {
-    for (i = 0; i < iFilterLen - j - 1; i++) {
-      if (bTab[i] > bTab[i + 1]) {
-        bTemp = bTab[i];
-        bTab[i] = bTab[i + 1];
-        bTab[i + 1] = bTemp;
+  if (ArduinoSerialCom.available() > 0) 
+  {
+    String cmd = ArduinoSerialCom.readString();
+    DynamicJsonDocument doc(200);
+    DeserializationError error = deserializeJson(doc, cmd);
+    if (error == DeserializationError::Ok) 
+    {
+      if (doc.containsKey("trigger") && doc["trigger"].is<String>()) 
+      {
+        if (doc["trigger"] == "tds") 
+        {
+          digitalWrite(tdsBufferRelay, LOW);
+          digitalWrite(LED_BUILTIN, HIGH);
+          delay(2000);
+          digitalWrite(tdsBufferRelay, HIGH);
+          digitalWrite(LED_BUILTIN, LOW);
+        }
       }
     }
   }
-  if ((iFilterLen & 1) > 0) {
-    bTemp = bTab[(iFilterLen - 1) / 2];
-  } else {
-    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
-  }
-  return bTemp;
-
 }
--- esp32_board/esp32_board.ino
+++ esp32_board/esp32_board.ino
@@ -1,5 +1,7 @@
 #include <WiFi.h>
 #include <WiFiManager.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 #include <SoftwareSerial.h>
@@ -9,14 +11,43 @@
 WiFiManager wifiManager;
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
-SoftwareSerial ArduinoSerialCom(35, 34);
+SoftwareSerial EspSerialCom(16, 17);
 
+// TDS
+#define TdsSensorPin 34  
+#define VREE 3.3
+#define SCOUNT 30
+
+int analogBuffer[SCOUNT];
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0;
+int copyIndex = 0;
+float averageVoltage = 0;
+float tdsValue = 0;
+
+// PH
+const int pin_pH = 35;  // Change to your desired analog pin
+int pH_analog;
+float Po = 0;
+float pH_step;
+double voltage;
+float PH4 = 3.22;
+float PH7 = 2.69;
+
+// Temp
+const int oneWireBus = 4;
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
+
+// Device Specific
 const char* DEVICE_ID = SECRET_DEVICE_ID;
 
+// Wifi
 const char* AP_SSID = "Hydroponix Device";
 const char* AP_PASSWORD = SECRET_AP_PASSWORD;
 bool isWiFiConnected = false;
 
+// Mqtt
 const char* MQTT_HOST = "test.mosquitto.org";
 const int MQTT_PORT = 1883;
 const char* MQTT_USERNAME = SECRET_MQTT_USERNAME;
@@ -33,11 +64,15 @@ void mqttCallback(char*, byte*, unsigned int);
 void mqttPumpHandler(String);
 void espNowRecvCallback(const uint8_t*, const uint8_t*, int);
 void publishSensorData(String);
+int getMedianNum(int[], int);
 
 void setup() 
 {
   Serial.begin(115200);
-  ArduinoSerialCom.begin(9600);
+  EspSerialCom.begin(9600);
+
+  pinMode(TdsSensorPin, INPUT);
+  sensors.begin();
 
   WiFi.mode(WIFI_STA);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -45,7 +80,7 @@ void setup()
   wifiManager.setDarkMode(true);
   wifiManager.setWiFiAutoReconnect(true);
 
-  if(!wifiManager.autoConnect(AP_SSID, AP_PASSWORD)) 
+  if (!wifiManager.autoConnect(AP_SSID, AP_PASSWORD)) 
   {
     Serial.println("Failed to connect");
   }
@@ -63,23 +98,66 @@ void setup()
 
 void loop() 
 {
-  if (ArduinoSerialCom.available() > 0) 
+  // Temp 
+  sensors.requestTemperatures();
+  float temperatureC = sensors.getTempCByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+
+  // TDS
+  static unsigned long analogSampleTime = millis();
+  if (millis() - analogSampleTime > 40U)
   {
-    String sensorData = ArduinoSerialCom.readString();
-
-    DynamicJsonDocument doc(200);
-    DeserializationError error = deserializeJson(doc, sensorData);
-
-    if (error == DeserializationError::Ok) 
+    analogSampleTime = millis();
+    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);
+    analogBufferIndex++;
+    if (analogBufferIndex == SCOUNT) 
     {
-      if (doc.containsKey("ph") && doc.containsKey("tds") && doc.containsKey("temp") &&
-          doc["ph"].is<float>() && doc["tds"].is<float>() && doc["temp"].is<float>()) 
-      {
-        Serial.print(sensorData);
-        publishSensorData(sensorData);
-      }
+      analogBufferIndex = 0;
     }
   }
+  static unsigned long printTimePoint = millis();
+  if (millis() - printTimePoint > 800U) 
+  {
+    printTimePoint = millis();
+    for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++) 
+    {
+      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+      averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREE / 4095.0; // For ESP32, ADC resolution is 12 bits (4095)
+      float compensationCoefficient = 1.0 + 0.02 * (temperatureC - 25.0);
+      float compensationVoltage = averageVoltage / compensationCoefficient;
+      tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
+      Serial.print("TDS Value: ");
+      Serial.print(tdsValue, 0);
+      Serial.println(" ppm");
+    }
+  }
+
+  // PH
+  pH_analog = analogRead(pin_pH);
+  Serial.print("ADC Value: ");
+  Serial.println(pH_analog);
+  
+  // Calculate voltage
+  voltage = pH_analog * (3.3 / 4095.0);  // For ESP32, ADC resolution is 12 bits (4095)
+  Serial.print("Voltage: ");
+  Serial.println(voltage, 4);
+
+  // Calculate pH value of the liquid
+  pH_step = (PH4 - PH7) / 3;
+  Po = 7 + ((PH7 - voltage) / pH_step);
+  Serial.print("pH Value: ");
+  Serial.println(Po, 2);
+
+  DynamicJsonDocument SensorJDoc(JSON_OBJECT_SIZE(3));
+
+  SensorJDoc["temp"] = temperatureC;
+  SensorJDoc["tds"] = tdsValue;
+  SensorJDoc["ph"] = Po;
+
+  String sensorJsonFormat;
+  serializeJson(SensorJDoc, sensorJsonFormat);
+  publishSensorData(sensorJsonFormat);
 }
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) 
@@ -88,7 +166,8 @@ void mqttCallback(char* topic, byte* payload, unsigned int length)
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
 
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++) 
+  {
     msgBuff += (char) payload[i];
   }
 
@@ -97,9 +176,9 @@ void mqttCallback(char* topic, byte* payload, unsigned int length)
 
 void monitorWiFiConnection(void* params)
 {
-  while(true)
+  while (true)
   {
-    if(WiFi.isConnected()) 
+    if (WiFi.isConnected()) 
     {
       digitalWrite(LED_BUILTIN, HIGH);
       isWiFiConnected = true;
@@ -109,7 +188,7 @@ void monitorWiFiConnection(void* params)
     {
       digitalWrite(LED_BUILTIN, LOW);
       isWiFiConnected = false;
-      if(!wifiManager.getConfigPortalActive()) wifiManager.startConfigPortal(AP_SSID, AP_PASSWORD);
+      if (!wifiManager.getConfigPortalActive()) wifiManager.startConfigPortal(AP_SSID, AP_PASSWORD);
     }
     vTaskDelay(250 / portTICK_PERIOD_MS);
   }
@@ -117,16 +196,16 @@ void monitorWiFiConnection(void* params)
 
 void connectToMqttBroker(void* params)
 {
-  while(true)
+  while (true)
   {
-    if(isWiFiConnected && mqttClient.connected())
+    if (isWiFiConnected && mqttClient.connected())
     {
       mqttClient.loop();
     }
-    else if(isWiFiConnected)
+    else if (isWiFiConnected)
     {
-       Serial.println("Attempting to connect MQTT broker");
-      if(mqttClient.connect(DEVICE_ID, MQTT_USERNAME, MQTT_PASSWORD))
+      Serial.println("Attempting to connect MQTT broker");
+      if (mqttClient.connect(DEVICE_ID, MQTT_USERNAME, MQTT_PASSWORD))
       {
         Serial.println("Connected to MQTT broker");
         mqttClient.subscribe(PUMP_TOPIC);
@@ -142,14 +221,44 @@ void connectToMqttBroker(void* params)
 
 void mqttPumpHandler(String msg)
 {
-  // Triggering pump
-  Serial.println(msg);
+  EspSerialCom.println(msg);
 }
 
 void publishSensorData(String jsonFormattedSensorData) 
 {
-  if(mqttClient.connected())
+  if (mqttClient.connected())
   {    
     mqttClient.publish(SENSOR_TOPIC, jsonFormattedSensorData.c_str());
   }
+}
+
+int getMedianNum(int bArray[], int iFilterLen) 
+{
+  int bTab[iFilterLen];
+  for (byte i = 0; i < iFilterLen; i++) 
+  {
+    bTab[i] = bArray[i];
+  }
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++) 
+  {
+    for (i = 0; i < iFilterLen - j - 1; i++) 
+    {
+      if (bTab[i] > bTab[i + 1]) 
+      {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0) 
+  {
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  } 
+  else 
+  {
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  }
+  return bTemp;
 }
